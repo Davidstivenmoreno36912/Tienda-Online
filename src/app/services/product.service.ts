9ee42ba1a245719a5972aca71348f95b2326967e
Davidstivenmoreno36912 <Davidstivenmoreno36912@gmail.com>
2025-10-01 13:18:13 -0500

    Update product.service.ts

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -2,16 +2,16 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Product } from '../models/product.model';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
-  private apiUrl = 'https://backend-41mc.onrender.com/products'; // URL del backend API
+  private apiUrl = `${environment.apiUrl}/products`; // ahora sí usa environment
 
   constructor(private http: HttpClient) { }
 
-  // Método para obtener todos los productos desde el backend
   getProducts(): Observable<Product[]> {
     return this.http.get<Product[]>(this.apiUrl);
   }
